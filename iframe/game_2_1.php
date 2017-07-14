81b2452349bc5ebfe798d8fc3f3c2d856be983f7
ap311036 <ap311036@gmail.com>
2017-07-15 01:14:40 +0800

    GameMoney
    Proj

---
+++ iframe/game_2_1.php
@@ -0,0 +1,30 @@
+<?php
+//var_dump($_SESSION['member_point']);
+//var_dump($_COOKIE['member_point']);
+//var_dump($_COOKIE['member_account']);
+$frame_value = $_GET['key'];
+include_once '../lib/config.php';
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript">
+//$(function(){
+//    $('#iframe1').ready(function(){
+//        var $iframe = $(this),
+//            $contents = $iframe.contents();
+//
+//            console.log($contents.find("#btn3").val());
+//        });
+//
+//
+//});
+  // $(function(){
+  //   setIframeSize();
+  //   function setIframeSize() {
+  //     var height = window.innerHeight - 130 + "px";
+  //     $("#iframe2").css({ "height" : height});
+  //   }
+  //   window.addEventListener("resize", setIframeSize);
+  // })
+</script>
+<div class="closeGame2">關閉遊戲</div>
+<iframe id="iframe2" name="iframe2" style="width:100%;height:100%" src="<?php echo $win7pkwebgl; ?>index2.html?user=<?php echo $_COOKIE['member_account']; ?>&key=<?php echo $frame_value ?>" scrolling="NO"></iframe>
---
+++ lib/config.php
@@ -0,0 +1,30 @@
+<?php
+
+
+$webroot = 'http://localhost/pokerweb/newweb/';
+//$adminroot = 'http://pokerweb.sammicorner.com/admin/';
+//$webglroot = 'http://poker.mudys.com/WebGL/TexasHoldemDev/';
+//$webglroot = 'http://60.250.122.220/tiger_slot/';
+//$FbAppid = '1181751815224247';//maicca
+//$FbAppid = '183891728751030';//etron
+//$FbAppkey = '08ba4df80f3d88ebf128c740b75e23c6';//maicca
+//$FbAppkey = 'ccacc826cc3b198362464b682c19d374';//etron
+//$reCapcha = '6LeCJgkUAAAAALdihUlYGIsdKJp_Ec7QdKJ4A1f9';//sammi
+//$wsserver =
+$reCapcha = '6LdsOg8UAAAAAElIsx5ABy_SeBWxMPSruCEfY4O7';//muduy.slot
+//
+$db_config = array();
+$db_config['ip'] = '127.0.0.1';
+$db_config['account'] = 'root';
+$db_config['password'] = 'root';
+$db_config['dbName'] = 'pokerweb';
+
+//$adb_config = array();
+//$adb_config['ip'] = '127.0.0.1';
+//$adb_config['account'] = 'root';
+//$adb_config['password'] = 'root';
+//$adb_config['dbName'] = 'tiger_slot';
+
+
+
+?>
